58c5bfeb3574c39d11012647d5fceaff98a0ccad
gendo37 <39395361+gend037@users.noreply.github.com>
2018-12-05 16:35:23 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,104 +5,54 @@ from discord.voice_client import VoiceClient
 import asyncio
 import time
 import random
-import os
-from discord import opus
 
-OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
- 
-def load_opus_lib(opus_libs=OPUS_LIBS):
-    if opus.is_loaded():
-        return True
- 
-    for opus_lib in opus_libs:
-            try:
-                opus.load_opus(opus_lib)
-                return
-            except OSError:
-                pass
- 
-    raise RuntimeError('Could not load an opus lib. Tried %s' % (', '.join(opus_libs)))
- 
 
- 
-load_opus_lib()
-bot = commands.Bot("")
-for ext in ["Music"]
-    bot.load_extension(ext)
-	
-startup_extensions = ["Music"]
+
 BOT_PREFIX = ("")
-TOKEN = "NDc3NDU3OTQyNzQwOTkyMDEx.DlAmXA.q5949JRoED9yZgIOEDT9vLPfb5o"
+TOKEN = "NTE5Nzc1MzIzMTA2MTgxMTQw.DukOUQ.Q_sgMp5ZojcFUBXiVXiagDUv4ME"
 
 @bot.event
 async def on_ready():
 	print('시작!')
 
 @bot.command()
-async def 냥이야():
+async def hello():
     possible_responses = [
-        '왜 불렀느냐',
-        '왜 그러느냐',
-        '무슨 문제있느냐',
+        'hi',
+        'how are you?',
+        'hi hi',
 
     ]
     await bot.say(random.choice(possible_responses))
 
 @bot.command()
-async def 안녕():
+async def blyat():
     possible_responses = [
-        '인사를 받아주겠느니라',
-        '갑자기 인사를 왜 하는것이냐',
-        '안녕하느냐',
+        'cyka blyat',
+        'omg',
+        'hahaha',
 
     ]
     await bot.say(random.choice(possible_responses))
+
 
 @bot.command()
-async def 고마워():
+async def cheeki breeki():
     possible_responses = [
-        '치...칭찬을 한다고..  내가 널 좋아할..것.. 같으냐..!.',
-        '칭찬해준다고 아무것도 나오지 않느니라',
-        '너 머리가 잘못된 것이냐 갑자기 이상한 말을 지껄이는 구나',
-        '흥, 내가 잘했으니 당연히 칭찬을 받아야 하는 것이니라',
+        'listen to stakler clear sky bandit radio',
+        'soviet',
+        'union',
+        'omg',
 
     ]
     await bot.say(random.choice(possible_responses))
-
-@bot.command()
-async def 놀아줘():
-    possible_responses = [
-        '그럴 시간에 우리 흰둥이나 신경쓰거라',
-        '쯧 쯧 그러니 니가 머리가 나쁜것이니라',
-        '귀찮느니라 혼자서 놀거라',
-        '난 내 소중한 흰둥이랑 놀아줘야하니 귀찮게 하지 말거라',
-
-    ]
-    await bot.say(random.choice(possible_responses))
-
-@bot.command()
-async def 시발():
-    possible_responses = [
-        '이런 미친놈이',
-        '욕 하면 죽인다',
-        '경고임 ㅅㄱ',
-
-    ]
-    await bot.say(random.choice(possible_responses))
-
-if __name__ == "__main__":
-        for extension in startup_extensions:
-            try:
-                bot.load_extension(extension)
-            except Exception as e:
-                exc = '{}: {}' .format(type(e).__name__, e)
-                print('failed to load extension {}/n{}'.format(extension, exc))
-
-        
-
-
-
-
+	
+filter = ["fuck","hell","Token"]
+	
+for filter in filter:
+    if filter in message:
+        await bot.send_message(message.channel, "{}, your message has been censored.".format(message.author.mention))
+        await bot.delete_message(message)
 
 
 
